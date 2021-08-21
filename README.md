66ec4313400c5a0e9f62f94e1407e2ded7cdd503
Mel Vint <melvint@Mels-MacBook-Pro.local>
2021-08-20 22:16:53 -0400

    updated.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Password-Generator
 generating a password
+
+
+## Table of Contents
+
+1.[Description](#Description)
+
+2.[Screen Shots](#Screenshots)
+
+3.[License](#License)
+
+4.[Usage](#Usage)
+
+5.[Badges](#Badges)
+
+## Description
+A trial of a Passcode-Generator
+
+## Screenshots
+![Password-Generator](assets/images/)
+![Password-Generator](assets/images/)
+![Password-Generator](assets/images/)
+![Password-Generator](assets/images/)
+
+=======
+
+## License
+(MIT)
+
+## Usage
+
+## Badges
+(HTML, Style.CSS)
+
---
+++ assets/Develop/script.js
@@ -0,0 +1,86 @@
+var GenerateBtn = document.querySelector("#generate");
+var number = "0123456789";
+var special = "!@#$%^&*()<=_-`>?:;{|}~";
+var letter = "abcdefghijklmnopqrstuvwxyz";
+var Uppercaseletter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var stored = []; //----------------------------------------
+
+var hasUppercaseletter = true;
+var hasNumber = true;
+var hasLetter = true;
+var hasSpecial = true;
+
+var length = 34;
+
+hasUppercase = confirm('Uppercaseletter');
+hasSpecial = confirm('Special');
+hasLetter = confirm('Letter');
+hasNumber = confirm('Number');
+
+function generatepasscode() {
+    
+    var passcode = "";
+    var possible = "";
+
+
+    if  (hasLetter) {
+    var random = Math.floor(Math.random() * letter.length);
+    var selectedletter = letter[random];
+    stored.push(selectedletter);
+    possible = possible.concat(letter);
+    console.log(selectedletter);
+}
+//----------------------------------------------------------
+if (hasUppercaseletter) {
+    var random = Math.floor(Math.random() * Uppercaseletter.length);
+    var selectedUppercaseletter = Uppercaseletter[random];
+    possible = possible.concat(Uppercaseletter);
+    stored.push(selectedUppercaseletter);
+    console.log(selectedUppercaseletter);
+}
+//----------------------------------------------------------
+
+if (hasSpecial) {
+    var random = Math.floor(Math.random() * special.length);
+    var selectedspecial = special[random];
+    possible = possible.concat(special);
+    stored.push(selectedspecial);
+    console.log(selectedspecial);
+}
+
+if (hasNumber) {
+    var random = Math.floor(Math.random() * number.length);
+    var selectedNumber = [random];
+    possible = possible.concat(number);
+    stored.push(selectedNumber);
+    console.log(selectedNumber);
+}
+
+
+    for (var i = 0; i < length - stored.length; i++) {
+        var random = Math.floor(Math.random() * letter.length);
+        passcode += letter[random];
+    }
+        passcode += stored.join("");
+    
+        console.log (passcode);
+        
+     
+        console.log(stored);
+        stored = [];
+
+
+        return passcode;
+        
+    }
+//----------------------------------------------------------
+    function writepasscode() {
+     var passcode = generatepasscode();
+     var passcodeText = document.querySelector("#passcode");
+
+     if (passcode){
+         passcodeText.value = passcode;
+}}
+
+ GenerateBtn.addEventListener("click", writepasscode);
